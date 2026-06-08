e457bd1817341402d2b65f56471a563ffd603e66
FelipeMovio <felipelsmovio@gmail.com>
2026-06-08 10:25:54 -0300

    produtos entity create

---
+++ src/main/java/dev/felipe/restaurante/domain/entity/Produtos.java
@@ -0,0 +1,54 @@
+package dev.felipe.restaurante.domain.entity;
+
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "produtos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Produtos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private BigDecimal preco;
+
+    private Boolean disponivel = true;
+
+    @Column(name = "tempo_preparo_minutos")
+    private Integer tempoPreparoMinutos;
+
+    @Column(name = "criado_em")
+    private LocalDateTime criadoEm;
+
+    @Column(name = "atualizado_em")
+    private LocalDateTime atualizadoEm;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoria_id")
+    private CategoriaProduto categoria;
+
+    @PrePersist
+    public void Prepersist(){
+        criadoEm = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        atualizadoEm = LocalDateTime.now();
+    }
+}
